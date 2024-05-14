68c69eec1a65a3803e5b65640d6a0fc1f2bc33d0
Sohee Hwang <122145055+SOHEE990302@users.noreply.github.com>
2024-05-13 19:51:21 -0700

    re-add position function on signup.html

--- app/static/signup.js
+++ app/static/signup.js
@@ -1,29 +1,36 @@
-        function toggleVisibility() {
-            var accountType = document.getElementById("account_type").value;
-            var organizationNameDiv = document.getElementById("organization_name_field");
-            var organizationLabel = document.getElementById("organization_label");
+function toggleVisibility() {
+    var accountType = document.getElementById("account_type").value;
+    var organizationNameDiv = document.getElementById("organization_name_field");
+    var organizationLabel = document.getElementById("organization_label");
+    var positionField = document.getElementById("position_field");
 
-            if (["company", "school", "volunteer"].includes(accountType)) {
-                organizationNameDiv.style.display = "block";
-                organizationLabel.textContent = getLabelForType(accountType);
-            } else {
-                organizationNameDiv.style.display = "none";
-            }
-        }
+    if (["company", "school", "volunteer"].includes(accountType)) {
+        organizationNameDiv.style.display = "block";
+        organizationLabel.textContent = getLabelForType(accountType);
+    } else {
+        organizationNameDiv.style.display = "none";
+    }
 
-        function getLabelForType(type) {
-            switch (type) {
-                case 'company':
-                    return 'Name of your Company:';
-                case 'school':
-                    return 'Name of your School:';
-                case 'volunteer':
-                    return 'Name of your Volunteer Organization:';
-                default:
-                    return 'Name:';
-            }
-        }
+    if (accountType === "school") {
+        positionField.style.display = "block";
+    } else {
+        positionField.style.display = "none";
+    }
+}
 
-        function redirectToSearch() {
-            window.location.href = 'search.html'; // Update this URL as needed for your project
-        }
+function getLabelForType(type) {
+    switch (type) {
+        case 'company':
+            return 'Name of your Company:';
+        case 'school':
+            return 'Name of your School:';
+        case 'volunteer':
+            return 'Name of your Volunteer Organization:';
+        default:
+            return 'Name:';
+    }
+}
+
+function redirectToSearch() {
+    window.location.href = 'search.html'; // Update this URL as needed for your project
+}
