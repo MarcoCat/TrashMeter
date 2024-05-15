4576c4f2132c9f7b681e50de59779d55b2b66a8a
Marco <marcopoon217@gmail.com>
2024-05-15 04:30:41 -0700

    changes position to be nullable

--- app/models.py
+++ app/models.py
@@ -8,5 +8,5 @@ class User(db.Model):
     last_name = db.Column(db.String(100), nullable=False)
     email = db.Column(db.String(100), unique=True, nullable=False)
     account_type = db.Column(db.String(100), nullable=False)
-    position = db.Column(db.String(100), nullable=False)
+    position = db.Column(db.String(100), nullable=True)
     trash_collected = db.Column(db.Integer, default=0)
