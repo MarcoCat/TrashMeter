de3a9a9fa87fcb90900b6663f7d36dbdd8382005
Sohee Hwang <122145055+SOHEE990302@users.noreply.github.com>
2024-05-16 02:17:39 -0700

    sohee

---
+++ app/static/js/nav.js
@@ -0,0 +1,9 @@
+// nav.js
+function toggleMenu() {
+    var navList = document.getElementById('navList');
+    if (navList.style.display === 'block') {
+        navList.style.display = 'none';
+    } else {
+        navList.style.display = 'block';
+    }
+}
