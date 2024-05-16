22146648a64ee5472c03ef07e2f7790c4090dfb1
Sohee Hwang <122145055+SOHEE990302@users.noreply.github.com>
2024-05-15 19:58:41 -0700

    Remove the location (teacher and student) that appears when you select a school account type on the subscription page and profile page,

--- app/static/js/signup.js
+++ app/static/js/signup.js
@@ -1,20 +1,17 @@
+document.addEventListener("DOMContentLoaded", function() {
+    toggleVisibility();
+});
+
 function toggleVisibility() {
     var accountType = document.getElementById("account_type").value;
     var organizationNameDiv = document.getElementById("organization_name_field");
     var organizationLabel = document.getElementById("organization_label");
-    var positionField = document.getElementById("position_field");
 
     if (["company", "school", "volunteer"].includes(accountType)) {
         organizationNameDiv.style.display = "block";
         organizationLabel.textContent = getLabelForType(accountType);
     } else {
         organizationNameDiv.style.display = "none";
-    }
-
-    if (accountType === "school") {
-        positionField.style.display = "block";
-    } else {
-        positionField.style.display = "none";
     }
 }
 
@@ -34,4 +31,13 @@ function getLabelForType(type) {
 function redirectToSearch() {
     var searchTerm = document.getElementById("organization_name").value;
     window.location.href = 'search.html?query=' + encodeURIComponent(searchTerm); // Pass search term as a query parameter
+}
+
+function previewImage(event) {
+    var reader = new FileReader();
+    reader.onload = function() {
+        var output = document.getElementById('profileImagePreview');
+        output.src = reader.result;
+    };
+    reader.readAsDataURL(event.target.files[0]);
 }
